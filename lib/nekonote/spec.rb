0bed9f26d4af2815ce5c2d877ba968dbc14899dd
khotta <khotta116@gmail.com>
2018-07-29 18:35:19 +0000

    beta5

--- lib/nekonote/spec.rb
+++ lib/nekonote/spec.rb
@@ -1,6 +1,6 @@
 module Nekonote
     LIBS_NAME   = %(nekonote-framework).freeze
-    VERSION     = %(1.0.0-beta4).freeze
+    VERSION     = %(1.0.0-beta5).freeze
     HOMEPAGE    = %(https://nekonote-framework.github.io).freeze
     SUMMARY     = %(Simple and Lightweight Ruby Framework for Web Development).freeze
     DESCRIPTION = %(Nekonote Framework is a small web application framework that helps your web development.).freeze
